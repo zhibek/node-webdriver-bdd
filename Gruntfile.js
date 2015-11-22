f4cd4a41651afe96de740329783a026c18334928
Mohamed Labib <lebaz20@gmail.com>
2015-11-22 15:22:02 +0200

    Jasmin prototype one - run via command : npm install && node_modules/.bin/grunt -- still not capable of opening google page and searching through it

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'                
+            }
+        }
+    });
+
+    // Load the plugin that provides the "karma" task.
+    grunt.loadNpmTasks('grunt-karma');
+
+    // Default task(s).
+    grunt.registerTask('default', ['karma']);
+
+};
